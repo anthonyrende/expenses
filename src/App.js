428f0c49c923e4d625cd71c9c640a01aa583aaea
ren.meta@gmail.com <ren.meta@gmail.com>
2020-08-31 08:52:46 -0400

    Markup added to dumb components without logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Header from "./components/Header.js"
+import { Balance } from "./components/Balance"
+import { IncomeExpenses } from "./components/IncomeExpenses"
+import { TransactionList } from "./components/TransactionList"
+import { AddTransaction } from "./components/AddTransaction"
 
 function App() {
   return (
     <div className="App">
-    yo
+    <Header />
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
     </div>
   );
 }
