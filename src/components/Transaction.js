da5e5a6f3d9d57118db635cc6e6bb1121ac55c53
ren.meta@gmail.com <ren.meta@gmail.com>
2020-09-12 02:28:51 -0400

    Transaction component dynamically gives + or - sign to a positive or negative transaction amount

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export const Transaction = ({ transaction }) => {
+    // If transaction is - or + 
+    const sign = transaction.amount < 0 ? '-' : '+';
+    
     return (
+        // TODO: Class ternary is dynamically positive o negative 
         <li>
-        {transaction.text} <span>-$5</span><button>x</button>
+        {transaction.text}<span> {sign} ${Math.abs(transaction.amount)}</span><button>x</button>
         </li>
     )
 }
