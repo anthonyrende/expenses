55ec03c90eeed5ea20d5110a4f6e39eea04cf41e
ren.meta@gmail.com <ren.meta@gmail.com>
2020-09-12 02:27:30 -0400

    Balance dynamically takes in transaction amounts from global state and adds them to total

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const Balance = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    // Grab all transaction amounts into amounts
+    const amounts = transactions.map(transaction => transaction.amount)
+    // Add them to a fixed decimal place of .2
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2)
+
     return (
         <div>
-            <h4>Balance</h4>
-            <h1>$0:00</h1>
+            <h4>Balance{amounts}</h4>
+            <h1>${total}</h1>
         </div>
     )
 }
