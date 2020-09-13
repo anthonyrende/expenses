596ff402757eaaf3a245d3638f6556a2bec2074c
ren.meta@gmail.com <ren.meta@gmail.com>
2020-09-13 15:43:33 -0400

    Imported Arwes style library

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
+
+// Components
 import Header from "./components/Header.js"
 import { Balance } from "./components/Balance"
 import { IncomeExpenses } from "./components/IncomeExpenses"
 import { TransactionList } from "./components/TransactionList"
 import { AddTransaction } from "./components/AddTransaction"
 
+// Context
 import { GlobalProvider } from "./context/GlobalState"
+
+// Theme
+import { ThemeProvider, createTheme, Arwes } from 'arwes';
+// import './style/arwes/expensesMain.css'
 
 function App() {
   return (
     <GlobalProvider>
-      <Header />
-      <Balance />
-      <IncomeExpenses />
-      <TransactionList />
-      <AddTransaction />
+      <ThemeProvider theme={createTheme()}>
+      <Arwes animate background='./images/wallpaper.jpg' pattern='./images/glow.png'>
+          
+          <Balance />
+          <IncomeExpenses />
+          <TransactionList />
+          <AddTransaction />
+        </Arwes>
+      </ThemeProvider>
     </GlobalProvider>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Arwes } from 'arwes'
+
 
 const Header = () => {
+    // const resources = {
+    //     bg: '../images/background.jpg',
+    //     pattern: '../images/glow.png',
+    // };
     return (
-        <div>
-            <h1>Header</h1>
+        <Arwes >
+        <div style={{ padding: 20 }}>
+            <Header animate>
+                <h1 style={{ margin: 0 }}>Expenses</h1>
+            </Header>
         </div>
+    </Arwes>
     )
 }
 
